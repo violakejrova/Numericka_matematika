864043ab4518d8bc0d1462435b2e9d26e186f9e8
Viola Kejřová <88209579+violakejrova@users.noreply.github.com>
2025-04-04 11:25:26 +0200

    Add files via upload

---
+++ newtonova_metoda.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+import math
+
+def u(x):
+    u = (math.exp(x) - x*math.exp(x) + 2 - math.e)
+    return u
+def du(x):
+    du = -x*math.exp(x)
+    return du
+
+# Newton
+x0 = 1/2
+for i in range (667):   
+    x1 = x0 - (u(x0) / du(x0))
+    x0 = x1
+print(x1)
+print(u(x1)) #ověření, že je to kořem
+
