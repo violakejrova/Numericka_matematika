71de175c99813b919793e3391457b0fb3192bc89
Viola Kejřová <88209579+violakejrova@users.noreply.github.com>
2025-06-30 22:30:40 +0200

    Update newtonova_metoda.py

--- newtonova_metoda.py
+++ newtonova_metoda.py
@@ -1,17 +1,22 @@
 import math
 
 def u(x):
-    u = (math.exp(x) - x*math.exp(x) + 2 - math.e)
-    return u
+    return - (x - 2) * math.exp(x) + (2 - math.e) * x - 2
+    
 def du(x):
-    du = -x*math.exp(x)
+    du = - (x - 1) * math.exp(x) + (2 - math.e)
     return du
+    
+def duu(x):
+    duu = - x * math.exp(x)
+    return duu
 
 # Newton
 x0 = 1/2
-for i in range (667):   
-    x1 = x0 - (u(x0) / du(x0))
+for i in range (666):   
+    x1 = x0 - (du(x0) / duu(x0))
     x0 = x1
 print(x1)
-print(u(x1)) #ověření, že je to kořen
+print(u(x1))
+print(f"u v maximu: {u(x1)}")
 
